b1d459e0dd73adedbf198be8d8c0468e03472ec5
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-08-15 00:53:16 +0200

    coded static html pages

---
+++ js/form.js
@@ -0,0 +1,16 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+  const emailField = document.getElementById("user-email");
+  const userEmail = emailField.value;
+//   console.log(userEmail);
+
+  const passwordField = document.getElementById("user-password");
+  const userPassword = passwordField.value;
+//   console.log(userPassword);
+
+if(userEmail === 'sujonsarder92@gmail.com' && userPassword === 'sujonsarder'){
+    window.location.href = 'bank.html';
+}
+else{
+    console.log('invalid email and password');
+}
+});
